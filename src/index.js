ffedf667bd021be3b32595554459eb7f4893d6c8
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-21 20:09:36 -0800

    Initialize default local storage items only if new device

--- src/index.js
+++ src/index.js
@@ -7,51 +7,54 @@ import {
   displayTodoTabsOfCurrentProject,
 } from "./dom.js";
 
-// SET LOCAL STORAGE
-const todoList = [
-  createTodo(
-    '0',
-    '0',
-    'Default Todo',
-    '2024-01-31',
-    'Notes show here',
-    'high',
-    'closed'
-  ),
-  createTodo(
-    '1',
-    '1',
-    'First Todo Item for Sub-project',
-    '2024-01-16',
-    'Some random note for this todo',
-    'medium',
-  ),
-  createTodo(
-    '1',
-    '2',
-    'Second Todo Item for Sub-project',
-    '2024-02-22',
-    'Things I gotta remember about this task',
-    'medium',
-  ),
-];
-const projectList = [
-  createProject(
-    '0',
-    "All My Todos", 
-    "Project containing all my todo's"
-  ),
-  createProject(
-    '1',
-    "Another Project", 
-    "Project containing todos for another project"
-  ),
-];
-let currentProject = projectList[0];
 
-localStorage.setItem('todoList', JSON.stringify(todoList));
-localStorage.setItem('projectList', JSON.stringify(projectList));
-localStorage.setItem('currentProject', JSON.stringify(currentProject));
+// INITIALIZE LOCAL STORAGE
+if (!localStorage.length) {
+  const todoList = [
+    createTodo(
+      '0',
+      '0',
+      'Default Todo',
+      '2024-01-31',
+      'Notes show here',
+      'high',
+      'closed'
+    ),
+    createTodo(
+      '1',
+      '1',
+      'First Todo Item for Sub-project',
+      '2024-01-16',
+      'Some random note for this todo',
+      'medium',
+    ),
+    createTodo(
+      '1',
+      '2',
+      'Second Todo Item for Sub-project',
+      '2024-02-22',
+      'Things I gotta remember about this task',
+      'medium',
+    ),
+  ];
+  const projectList = [
+    createProject(
+      '0',
+      "All My Todos", 
+      "Project containing all my todo's"
+    ),
+    createProject(
+      '1',
+      "Another Project", 
+      "Project containing todos for another project"
+    ),
+  ];
+  let currentProject = projectList[0];
+
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  localStorage.setItem('projectList', JSON.stringify(projectList));
+  localStorage.setItem('currentProject', JSON.stringify(currentProject));
+}
 
 
 // RENDER DEFAULT UI
